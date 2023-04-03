dfefc17951fd25eba8364b26b050555093d2145c
angel <103801832+angelrmdhn@users.noreply.github.com>
2023-04-03 17:00:40 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Calculator'),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    title: "Belajar Flutter",
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppcreateState createState() => _MyAppcreateState();
+}
+
+class _MyAppcreateState extends State<MyApp> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen,
+          title: Text(
+            'Calculator',
+            style: TextStyle(fontSize: 22.0),
+          ),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Result: 0',
+                    style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+              child: TextFormField(
+                decoration: new InputDecoration(
+                    hintText: 'Enter First Number',
+                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0))),
+              ),
+            ),
+            SizedBox(height: 25.0),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+                decoration: new InputDecoration(
+                    hintText: 'Enter Second Number',
+                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(12.0))),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style:
+                          ElevatedButton.styleFrom(primary: Colors.lightGreen),
+                      onPressed: () {},
+                      child: Text(
+                        'ADD',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.normal),
+                      )),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.lightGreen,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'SUBSTRACT',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.normal),
+                      )),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.lightGreen,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'MULTIPLY',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.normal),
+                      )),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.lightGreen,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'DIVIDE',
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.normal),
+                      )),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
